346dfa92fac1c2b116578e9d466b837ac848cc50
UzairJ99 <uzair-jawaid@hotmail.com>
2021-05-12 23:03:44 -0400

    added send button to frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 
 const main = async () => {
+    var bodyParser = require('body-parser')
+
     const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+
     const PORT = process.env.PORT || 5000;
 
     // landing page - go to localhost:5000 to test if this shows up
     app.get('/', (_req, res)=> {
         res.send('hello world!');
+    })
+
+    // testing route trigger
+    app.get('/trigger', (req, res) => {
+        console.log("just got triggered.");
     })
 
     // activate app on port
--- extension/webviews/components/ChatList.svelte
+++ extension/webviews/components/ChatList.svelte
@@ -1,2 +1,26 @@
-<h1>Chats</h1>
-<p>we got it working boyz.</p>
+<script>
+    import axios from 'axios';
+
+    function sendMsg() {
+        console.log("die!!!!");
+
+        //send to backend
+        axios.get('/trigger').then(response => {
+            console.log(response);
+        });
+    }
+</script>
+
+<h1>Chat</h1>
+<!-- unordered list -->
+<ul>
+    <!-- mesages will be stored as list items to display -->
+    <li>hi</li>
+    <li>hello</li>
+</ul>
+
+
+<div><input id="messageBox" type="text"></div>
+<button on:click={() => sendMsg()}>SEND</button>
+
+
