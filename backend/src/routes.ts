323c3b342d373f87f95101d6c53a63f25ae93a46
Labeeb Zaker <zakerl@mcmaster.ca>
2021-07-06 02:06:37 -0400

    added authentication successfully and storing user sessions

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,13 +17,4 @@ app.get('/', (_req, res)=> {
     res.send("HI");  
 });
 
-// send message from the textbox to the database
-app.post('/sendMessage', (req, res) => {
-    // extract text message from the body of the request sent
-    let message = req.body;
-    console.log(message);
-    // replace with socket.io functionality and database functions
-    res.send(message);
-});
-
 export default app
