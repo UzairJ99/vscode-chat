ac1f004d888cef84e65b0e83a19f87c264d11f2b
Marium Khan <mkarium12@gmail.com>
2021-07-07 23:51:14 -0400

    updated login page styling

--- extension/webviews/components/Login.svelte
+++ extension/webviews/components/Login.svelte
@@ -1,4 +1,8 @@
-
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+</svelte:head>
 <script>
     import {push, pop, replace} from 'svelte-spa-router';
     import io from "socket.io-client";
@@ -12,12 +16,14 @@
         //     console.log(err);
         // }
         
-        console.log(response);
+        // console.log(response);
         push('/Chat');
     }
 </script>
 <div id="background">
-    VS CHAT
+    <div id="title">
+        <h1 id="vschat">VS CHAT</h1>
+    </div>
     <div id="card">
         <h2 id="cardTitle">GitHub Sign In</h2>
         <button id="login" on:click|preventDefault={navigate}><div id="githubLogo"></div></button>
